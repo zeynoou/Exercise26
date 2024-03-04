09f7148f6ba26d3de7259dadd70d7172f4809864
Zeynab <Zeynab@ZEYNAB>
2024-03-04 10:56:07 +0330

    Add project files.

---
+++ Exercise26/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise26
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[,] anArray = new int[,] { { 1, 5, 7, 4 }, { 1, 3, 12, 2 }, { 23, 25, 12, 13 } };
+            foreach (int i in anArray)
+            {
+                var number = anArray.Select((i) => i).Max();
+                Console.WriteLine($"Maximum number = {number}");
+            }
+            
+        }
+    }
+}
